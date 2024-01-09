c1b2c4088c5429f29af127633178089329bae531
sonnka <sonyakazanceva1331@gmail.com>
2024-01-09 14:09:14 +0200

    Added register page and navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,5 @@ export class AppComponent {
   title = 'VigiDriveWeb';
   login = 'Login';
   register = 'Register';
+  home = 'Home';
 }
