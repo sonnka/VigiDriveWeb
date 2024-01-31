63454f4c4aaf3cffc7ddae4003694419f093fa2f
sonnka <sonyakazanceva1331@gmail.com>
2024-01-31 16:42:54 +0200

    Some refactoring of registration page and health diagrams

--- src/app/_models/register.request.ts
+++ src/app/_models/register.request.ts
@@ -3,13 +3,11 @@ export class RegisterRequest {
   lastName: string;
   email: string;
   password: string;
-  avatar: string;
 
-  constructor(firstName: string, lastName: string, email: string, password: string, avatar: string) {
+  constructor(firstName: string, lastName: string, email: string, password: string) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.password = password;
-    this.avatar = avatar;
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,16 +14,16 @@ export class RegisterComponent {
   constructor(private driverService: DriverService, private managerService: ManagerService, private router: Router) {
   }
 
-  registerDriver(firstName: string, lastName: string, email: string, password: string, avatar: string): void {
-    let data = new RegisterRequest(firstName, lastName, email, password, avatar);
+  registerDriver(firstName: string, lastName: string, email: string, password: string): void {
+    let data = new RegisterRequest(firstName, lastName, email, password);
     this.driverService.register(data).subscribe(response => {
       console.log(response);
     });
     this.router.navigate(['/login']);
   }
 
-  registerManager(firstName: string, lastName: string, email: string, password: string, avatar: string): void {
-    let data = new RegisterRequest(firstName, lastName, email, password, avatar);
+  registerManager(firstName: string, lastName: string, email: string, password: string): void {
+    let data = new RegisterRequest(firstName, lastName, email, password);
     this.managerService.register(data).subscribe(response => {
       console.log(response);
     });
